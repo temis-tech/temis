5337d1f7163ff9a6202b9cdde06a258438fdd64f
rainbowsay-tech <rainbowsay-tech@yandex.ru>
2025-12-14 10:59:15 +0100

    Добавлена автоматическая пауза видео при перелистывании карусели
    Проблема: при перелистывании карусели видео продолжало воспроизводиться.
    
    Исправление:
    1. Добавлены refs для всех video и iframe элементов
    2. При изменении carouselIndex все видео, кроме текущего, ставятся на паузу
    3. При переключении в полноэкранном режиме также ставится на паузу
    4. Поддержка postMessage API для YouTube, Rutube, Vimeo iframe
    
    Теперь при перелистывании карусели видео автоматически ставится на паузу.

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
 import { normalizeImageUrl } from '@/lib/utils'
 import { normalizeHtmlContent } from '@/lib/htmlUtils'
